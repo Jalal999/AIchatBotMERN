8cb4197bcf2182d499931bb4371a306546ec5460
Jalal999 <jmammadli2021@ada.edu.az>
2023-12-12 01:01:17 +0100

    implemented basic routes

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -1,8 +1,14 @@
 import express from "express";
 import { config } from "dotenv";
+import morgan from 'morgan';
+import appRouter from "./routes/index.js";
 config();
 const app = express();
-//middlewares
+//MIDDLEWARES
 app.use(express.json());
+//remove it in productuin
+app.use(morgan("dev"));
+//api structure for our application
+app.use("/api/v1", appRouter);
 export default app;
 //# sourceMappingURL=app.js.map
