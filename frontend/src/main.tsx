75819f21312aaa7368f840ebe2f3c6701203d5d9
Jalal999 <jmammadli2021@ada.edu.az>
2024-03-18 21:07:47 +0100

    adding toaster and cors

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,17 +4,19 @@ import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext.tsx'
+import { Toaster } from 'react-hot-toast'
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:5000/api/v1"
+axios.defaults.baseURL = "http://localhost:5555/api/v1"
 axios.defaults.withCredentials = true;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+      <BrowserRouter>
+        <Toaster position="top-right" />
+        <App />
+      </BrowserRouter>
     </AuthProvider>
   </React.StrictMode>,
 )
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import { Box, Typography, Button } from "@mui/material";
 import CustomizedInput from "../components/shared/CustomizedInput";
-import {IoIosLogIn} from 'react-icons/io'
+import {IoIosLogIn} from 'react-icons/io';
+import { toast } from 'react-hot-toast';
+import { useAuth } from "../context/AuthContext";
 
 const Login = () => {
+    const auth = useAuth();
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
       const formData = new FormData(e.currentTarget)
-      const email = formData.get("email")
-      const password = formData.get("password")
+      const email = formData.get("email") as string;
+      const password = formData.get("password") as string;
+      try {
+        toast.loading("Signing In", {id: "login"});
+        await auth?.login(email, password);
+        toast.success("Signed In Successfully", { id: "login"});
+      } catch (error) {
+        console.log(error);
+        toast.error("Signing In Failed", { id: "login" });
+      }
     }
     return (
         <Box width={"100%"} height={"100%"} display="flex" flex={1}>
