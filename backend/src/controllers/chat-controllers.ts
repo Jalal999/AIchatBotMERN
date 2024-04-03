d8ee384b1f5709c88637e9ae95919b430803512a
Jalal999 <jmammadli2021@ada.edu.az>
2024-04-03 20:28:47 +0200

    feat(chat): chat interaction is implemented
    - added routes to get and send chat message
    - added chat controllers accordingly to get and push new chat messages between user and GPT
    - implemented Chat page with sending message input and to show all messages between user and GPT

--- backend/src/controllers/chat-controllers.ts
+++ backend/src/controllers/chat-controllers.ts
@@ -30,3 +30,22 @@ export const generateChatComplation = async (req: Request, res: Response, next: 
         return res.status(500).json({message: "Something went wrong..."})
     }
 }
+
+export const sendChatsToUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        //user token check
+        const user = await User.findById(res.locals.jwtData.id);
+
+        if(!user) {
+            return res.status(401).send("User not registered or Token malfunctioned...")
+        }
+        if(user._id.toString() !== res.locals.jwtData.id) {
+            return res.status(401).send("Permissions did NOT match...")
+        }
+
+        return res.status(200).json({message: "OK", chats: user.chats })
+    } catch (error) {
+        console.log(error);
+        return res.status(404).json({message: "ERROR", cause: error.message})
+    }
+}
--- backend/src/routes/chat-routes.ts
+++ backend/src/routes/chat-routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { verifyToken } from "../utils/token-manager.js";
 import { chatCompletionValidator, validate } from "../utils/validators.js";
-import { generateChatComplation } from "../controllers/chat-controllers.js";
+import { generateChatComplation, sendChatsToUser } from "../controllers/chat-controllers.js";
 
 const chatRoutes = Router()
 
 chatRoutes.post("/new", validate(chatCompletionValidator), verifyToken, generateChatComplation)
+chatRoutes.get("/all-chats", verifyToken, sendChatsToUser)
 
 export default chatRoutes;
